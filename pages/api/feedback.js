5a308d2bfc478da6da288c7af2424978812f8d52
kanishkrawat17 <kanishkrawat67@gmail.com>
2024-01-20 16:43:59 +0530

    added mock db file and adding data in it

--- pages/api/feedback.js
+++ pages/api/feedback.js
@@ -1,18 +1,22 @@
+import fs from "fs";
+import path from "path";
 const handler = (req, res) => {
-    console.log(req.body, req.method, "Request")
-    if(req.method  == 'POST') {
-        const { username, password } = req.body;
-        if(username === "Kanishk17" && password == "1234") {
-            return res.status(200).json({
-                message: "Logged in :)"
-            })
-        } else {
-            return res.status(201).json({
-                message: "either password or username is incorrect...!!!!!"
-            })
-        }
-    }
-    
-}
+  console.log(req.body, req.method, "Request");
+  if (req.method == "POST") {
+    const { email, feedback } = req.body;
+    const pathname = path.join(process.cwd(), "/db/feedback.json");
+    const feedbackData = JSON.parse(fs.readFileSync(pathname, "utf-8")); // reading file synchronously.
+    feedbackData?.feedbacks?.push({ email, feedback });
+    fs.writeFileSync(pathname, JSON.stringify(feedbackData));
+    res.status(200).json({ message: "Success sent feedback" });
+  } else if(req.method == "GET") {
+    const pathname = path.join(process.cwd(), "/db/feedback.json");
+    const feedbackData = JSON.parse(fs.readFileSync(pathname, "utf-8")); // reading file synchronously.
+
+    return res.status(200).json({
+        data: feedbackData
+    })
+  }
+};
 
 export default handler;
--- pages/testing-api/index.js
+++ pages/testing-api/index.js
@@ -1,8 +1,10 @@
 import React, { useEffect, useRef, useState } from "react";
+import path from "path";
 import axios from "axios";
 
 const TestingApi = () => {
   const [data, setData] = useState(null);
+  const [feedbacks, setFeedbacks] = useState(null);
   const feedbackRef = useRef(null);
   const emailRef = useRef(null);
 
@@ -23,12 +25,20 @@ const TestingApi = () => {
     });
     const message = response?.data?.message ?? "";
     setData(message);
+    const pathname = path.join(process.cwd(), "/db/feedback.json");
+    console.log(pathname, "Hello");
   };
 
   const handleSubmit = () => {
     console.log("for submission");
   };
-  
+
+  const handleGetAllFeedbacks = async () => {
+    const response = await axios("http://localhost:3000/api/feedback");
+    const jsonData = response?.data?.data?.feedbacks ?? {};
+    setFeedbacks([...jsonData]);
+  };
+
   return (
     <>
       <h2>Status: {data ? data : "Loading......!!!"}</h2>
@@ -40,6 +50,17 @@ const TestingApi = () => {
         <textarea type="text" ref={feedbackRef} />
       </form>
       <button onClick={handleClick}>Submit feedback</button>
+      <button onClick={handleGetAllFeedbacks}>Get all feedbacks</button>
+      {feedbacks && feedbacks.length ? (
+          feedbacks.map((user) => {
+            return (
+              <ul>
+                <li>username: {user.email}</li>
+                <li>feedback: {user.feedback}</li>
+              </ul>
+            );
+          })
+      ) : null}
     </>
   );
 };
